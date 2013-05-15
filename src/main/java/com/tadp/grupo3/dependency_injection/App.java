35c278fd1892b2dd9db111f44b7af4ea74ddee74
Federico <federico.aloi@gmail.com>
2013-05-15 11:30:53 -0300

    Creado proyecto mavenizado

---
+++ src/main/java/com/tadp/grupo3/dependency_injection/App.java
@@ -0,0 +1,13 @@
+package com.tadp.grupo3.dependency_injection;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
