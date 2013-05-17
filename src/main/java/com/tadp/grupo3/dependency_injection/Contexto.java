9f09cb53a3c2dc83a2a330a06a7d2b6a9950b2e0
Rodrigo Alfonso <rodri042@gmail.com>
2013-05-16 22:44:05 -0300

    ''Estrategia''

--- src/main/java/com/tadp/grupo3/dependency_injection/Contexto.java
+++ src/main/java/com/tadp/grupo3/dependency_injection/Contexto.java
@@ -1,6 +1,12 @@
 package com.tadp.grupo3.dependency_injection;
 
+import java.lang.reflect.Constructor;
+import java.util.Map;
+import java.util.HashMap;
+
 public class Contexto {
+	
+	private Map<Class<?>, Object> bindings = new HashMap<Class<?>, Object>();
 
 	private static Contexto instance;
 
@@ -12,17 +18,39 @@ public class Contexto {
 	}
 
 	public void agregarBinding(Class<?> baseType, Class<?> concreteType) {
-		// TODO Auto-generated method stub
+		Object instancia = this.instanciarObjeto(concreteType);
+		
+		//Validar si ya lo tiene
+		this.bindings.put(baseType, concreteType);
 	}
 
 	public Object obtenerInstancia(Class<?> baseType) {
-			// TODO Auto-generated method stub
-			return null;
+		//Validar si no lo tiene
+		//this.bindings.get(baseType);
+
+		return null;
+	}
+	
+	public Object instanciarObjeto(Class<?> clazz) {
+		Constructor[] constructores = clazz.getConstructors();
+		//if (constructores.length != 1) explotar();
+		
+		/*
+		 * Invocar al unico constructor...
+		 * Si tiene parámetros, invocar antes por cada uno a this.obtenerInstancia(tipo)
+		 * 
+		 * Esto facilitaría un montón todo, pero tiene la contra de tener que bindear en orden
+		 * (no se si en los frameworks esto ya es así), pero si hay un:
+		 * - Perro
+		 * - CorreaParaPerro, que en el constructor recibe un perro
+		 * Si trataras de bindear primero CorreaParaPerro rompería al no tener la instancia de Perro
+		 */
+		
+		return null;
 	}
 
 	public void agregarBindingPrimitivo(Class<?> baseType, String string) {
 		// TODO Auto-generated method stub
-		
 	}
 
 }
