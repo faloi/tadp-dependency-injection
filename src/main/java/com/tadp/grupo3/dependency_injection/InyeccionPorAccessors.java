2029f33c3c73372e49c2f139854bbad7f4af4cb8
Rodrigo Alfonso <rodri042@gmail.com>
2013-05-30 23:15:49 -0300

    Ahora tengo el atributo. No sé como inyectarselo igual (?)

--- src/main/java/com/tadp/grupo3/dependency_injection/InyeccionPorAccessors.java
+++ src/main/java/com/tadp/grupo3/dependency_injection/InyeccionPorAccessors.java
@@ -4,7 +4,7 @@ import static ch.lambdaj.Lambda.filter;
 import static ch.lambdaj.Lambda.having;
 import static ch.lambdaj.Lambda.on;
 
-import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.List;
 
@@ -36,8 +36,27 @@ public class InyeccionPorAccessors implements EstrategiaInyeccion {
 		Class<?> clase = objeto.getClass();
 		
 		Method[] metodos = clase.getMethods();
-		List<Method> metodosAInyectar = filter(having(on(Method.class).getAnnotation(Inyectar.class) != null), metodos);
+		List<Method> metodosAnotadosParaInyectar = filter(having(on(Method.class).getAnnotation(Inyectar.class) != null), metodos);
+		List<Method> settersAInyectar = filter(having(on(Method.class).getName().startsWith("set")), metodosAnotadosParaInyectar);
 		
-		//sale café
+		for(Method setter : settersAInyectar) {
+			Field atributo;
+			try {
+				atributo = clase.getField(this.obtenerNombreDeAtributoDesdeBean(setter.getName()));
+			} catch (SecurityException e) {
+				throw new RuntimeException();
+			} catch (NoSuchFieldException e) {
+				throw new RuntimeException();
+			}
+			atributo.setAccessible(true);
+			
+			//?
+		}
+	}
+	
+	//¬.¬
+	private String obtenerNombreDeAtributoDesdeBean(String javaBeanMethod) {
+		String nombre = javaBeanMethod.substring(3);
+		return nombre.substring(0, 1).toLowerCase() + nombre.substring(1);
 	}
 }
